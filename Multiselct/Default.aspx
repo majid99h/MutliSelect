5d0fc48b5d6cb4d4e9c950b2a8cf2f0b1fae6fa2
Majid Nawaz <majid.nawaz@systemsltd.com>
2019-11-27 13:13:15 +0500

    First commit

---
+++ Multiselct/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Multiselct._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <link href="http://cdn.rawgit.com/davidstutz/bootstrap-multiselect/master/dist/css/bootstrap-multiselect.css" rel="stylesheet" type="text/css" />
+    <script src="http://cdn.rawgit.com/davidstutz/bootstrap-multiselect/master/dist/js/bootstrap-multiselect.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(function () {
+            $('[id*=userReg]').multiselect({
+                includeSelectAllOption: true,
+                enableFiltering: true
+            });
+        });
+    </script>
+    <asp:ListBox ID="userReg" runat="server" 
+        SelectionMode="Multiple"
+        DataValueField="RegistrationCode"
+         DataTextField="FirstName"
+         AutoPostBack="false">
+        
+    </asp:ListBox>
+    <asp:Button Text="Submit" runat="server" OnClick="Submit" />
+    <asp:Label ID="lblSelected" runat="server"></asp:Label>
+</asp:Content>
---
+++ Multiselct/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Multiselct
+{
+    public partial class _Default : Page
+    {
+        private readonly UserRegistrationEntities _dbContext = new UserRegistrationEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                userReg.DataSource = GetList();
+                userReg.DataBind();
+            }
+        }
+        private List<UserRegistration> GetList()
+        {
+            return _dbContext.UserRegistrations.ToList();
+        }
+        protected void Submit(object sender, EventArgs e)
+        {
+            string message = "";
+            foreach (ListItem item in userReg.Items)
+            {
+                if (item.Selected)
+                {
+                    message += $"{item.Value} | {item.Text},";
+                     
+                }
+            }
+            lblSelected.Text = message;
+            //ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", "alert('" + message + "');", true);
+        }
+    }
+}
